4dd27f797a407e0366cff0acb3a9870ac5ee5d99
Matheus Campos <matheus.camp32@gmail.com>
2023-07-28 10:58:54 -0300

    feat: finish check items and responsiviness screen

--- src/components/ListCard/index.js
+++ src/components/ListCard/index.js
@@ -1,21 +1,25 @@
-import { useState } from "react";
 import "./index.css";
 
-export const ListCard = ({ item, onClick }) => {
-  const [checked, setChecked] = useState(false);
-
+export const ListCard = ({ item, onClick, onCheckItem }) => {
   return (
-    <div className="list-card-container" onClick={() => onClick(item)}>
-      <img
-        className="checkbox"
-        src={`/images/${item?.checked ? "checked.svg" : "unchecked.svg"}`}
-        alt="checked-item"
-      />
+    <div className="list-card-container">
+      <div className="checkbox">
+        <input
+          onChange={() => onCheckItem(item)}
+          checked={item?.checked}
+          id="checkbox"
+          type="checkbox"
+        />
+
+        <label htmlFor="checkbox" />
+      </div>
       <div className="list-card-text-container">
         <span className="list-card-title">{item.name}</span>
-        <span className="list-card-subtitle">{item.quantity}</span>
+        <span className="list-card-subtitle">{item.quantity} unidades</span>
       </div>
-      <img src="/images/arrow.svg" alt="arrow-icon" className="arrow-icon" />
+      <div className="arrow-icon-container" onClick={() => onClick(item)}>
+        <img src="/images/arrow.svg" alt="arrow-icon" className="arrow-icon" />
+      </div>
     </div>
   );
 };
